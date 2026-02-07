815b5b513552ea259ac699d3fc27d13d2160c07b
muhammadsaman77 <samanmuhammad077@gmail.com>
2026-02-07 20:56:47 +0800

    feat: implement navbar in landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite"
+import analogjsangular from '@analogjs/astro-angular';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+      plugins: [tailwindcss()],
+    },
+  integrations: [analogjsangular()],
+});
